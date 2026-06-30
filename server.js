a5af074d41d3ed99d169edda6c0d2408a70ea63f
GCC Startup <dev@gccstartup.com>
2026-06-30 17:14:45 +0500

    Fix Express 5 path-to-regexp error for wildcard route

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post('/api/lead', async (req, res) => {
   }
 });
 
-app.get('*', (req, res) => {
+app.get(/^.*$/, (req, res) => {
   res.sendFile(path.join(__dirname, 'site', 'index.html'));
 });
 
